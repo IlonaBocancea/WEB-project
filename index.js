750bda41486dc76891def26bcbd1570f5f00981b
ilona <32567245+IlonaBocancea@users.noreply.github.com>
2019-03-04 22:04:38 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
-
-
 class Worker {
 	constructor(name, surname, hourRate, hours) {
 		this.name = name;
 		this.surname = surname;
 		this.hourRate = hourRate;
 		this.hours = hours;
-		var hours = 176;
-        var hourRate = 10;
+		
 }
 
 	}
 	
-	
 
-let worker = new Worker('Ion', 'Creanga', 10, 176);  
+let worker = new Worker('Ion', 'Creanga', 10, 176); 
 console.log(worker.name); 
 console.log(worker.surname);  
 console.log(worker.hourRate);  
 console.log(worker.hours);  
-console.log(worker.getSalary('hours'+'hourRate')); 
+console.log(worker.getSalary);
+console.log(10*176); 
 
+
+let worker = new Worker('Mihai', 'Eminescu', 15, 150); 
+console.log(worker.name); 
+console.log(worker.surname);  
+console.log(worker.hourRate);  
+console.log(worker.hours);  
+console.log(worker.getSalary);
+console.log(15*150); 
