dc35e5291dd3e2a1029c6e4362f5de365880f765
ilona <32567245+IlonaBocancea@users.noreply.github.com>
2019-04-15 13:10:27 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,28 +1,69 @@
-class Worker {
-	constructor(name, surname, hourRate, hours) {
-		this.name = name;
-		this.surname = surname;
-		this.hourRate = hourRate;
-		this.hours = hours;
-		
+'use strict'
+// Template version: 1.3.1
+// see http://vuejs-templates.github.io/webpack for documentation.
+
+const path = require('path')
+
+module.exports = {
+  dev: {
+
+    // Paths
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/',
+    proxyTable: {},
+
+    // Various Dev Server settings
+    host: 'localhost', // can be overwritten by process.env.HOST
+    port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    autoOpenBrowser: false,
+    errorOverlay: true,
+    notifyOnErrors: true,
+    poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
+
+    
+    /**
+     * Source Maps
+     */
+
+    // https://webpack.js.org/configuration/devtool/#development
+    devtool: 'cheap-module-eval-source-map',
+
+    // If you have problems debugging vue-files in devtools,
+    // set this to false - it *may* help
+    // https://vue-loader.vuejs.org/en/options.html#cachebusting
+    cacheBusting: true,
+
+    cssSourceMap: true
+  },
+
+  build: {
+    // Template for index.html
+    index: path.resolve(__dirname, '../dist/index.html'),
+
+    // Paths
+    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/',
+
+    /**
+     * Source Maps
+     */
+
+    productionSourceMap: true,
+    // https://webpack.js.org/configuration/devtool/#production
+    devtool: '#source-map',
+
+    // Gzip off by default as many popular static hosts such as
+    // Surge or Netlify already gzip all static assets for you.
+    // Before setting to `true`, make sure to:
+    // npm install --save-dev compression-webpack-plugin
+    productionGzip: false,
+    productionGzipExtensions: ['js', 'css'],
+
+    // Run the build command with an extra argument to
+    // View the bundle analyzer report after build finishes:
+    // `npm run build --report`
+    // Set to `true` or `false` to always turn it on or off
+    bundleAnalyzerReport: process.env.npm_config_report
+  }
 }
-
-	}
-	
-
-let worker = new Worker('Ion', 'Creanga', 10, 176); 
-console.log(worker.name); 
-console.log(worker.surname);  
-console.log(worker.hourRate);  
-console.log(worker.hours);  
-console.log(worker.getSalary);
-console.log(10*176); 
-
-
-let worker = new Worker('Mihai', 'Eminescu', 15, 150); 
-console.log(worker.name); 
-console.log(worker.surname);  
-console.log(worker.hourRate);  
-console.log(worker.hours);  
-console.log(worker.getSalary);
-console.log(15*150); 
